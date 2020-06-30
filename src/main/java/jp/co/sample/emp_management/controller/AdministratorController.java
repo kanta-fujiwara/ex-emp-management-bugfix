34d2a86986d0309f03611bcc3258c79538d43012
kanta-fujiwara <kanta.fujiwara@rakus.co.jp>
2020-06-30 16:04:53 +0900

    (2-1) ログイン中の管理者名を表示するように変更

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -110,6 +110,8 @@ public class AdministratorController {
 			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return toLogin();
 		}
+		
+		session.setAttribute("administratorName", administrator.getName());
 		return "forward:/employee/showList";
 	}
 	
